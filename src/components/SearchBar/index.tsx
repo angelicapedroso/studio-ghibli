83766ec01a537181d5bedb9ae514e2ef093e4e30
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-11 01:30:33 -0300

    chore: adiciona icons

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,3 +1,4 @@
+import { X, MagnifyingGlass } from 'phosphor-react';
 import './styles.css';
 
 function SearchBar({
@@ -8,12 +9,13 @@ function SearchBar({
 }) {
   return (
     <div className="container">
+      <MagnifyingGlass size={24} className="search-icon" />
       <input
         type="text"
         placeholder={placeholder}
         value={value}
         onChange={onChange} />
-      <button type="button" onClick={handleClick}>x</button>
+      <button type="button" onClick={handleClick}><X size={26} /></button>
     </div>
   );
 }
