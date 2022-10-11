8b50783eccea1b0d6e9dcfd70e27136681e8db6f
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-11 12:29:48 -0300

    refactor: css card films

--- src/components/FilmCard/index.tsx
+++ src/components/FilmCard/index.tsx
@@ -1,35 +1,27 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { IFilm } from '../../interfaces/Film';
 import DetailsButton from '../FilmDetailsButton';
 import './styles.css';
 
 function FilmCard({ films }: { films: IFilm[] }) {
-  const navigate = useNavigate();
-
-  const handleClick = (filmId: string | undefined) => {
-    navigate(`/details-film/${filmId}`);
-  };
-
   return (
     <div className="card-section">
-      <h1>Films</h1>
-      <div className="card" >
+      <h2 className="page-title">Films</h2>
+      <div className="card-grid" >
         {
           films.map((film) => (
             <div className='card-container'>
-            <img src={film.image} alt={film.title} className='card-banner' />
-            <div className='card-content'>
-              <div className='card-title-container'>
-                <h2 className='card-title'>{film.title}</h2>
-                <p className='card-director'>{film.director}</p>
-                <button type='button' className='card-button'>Details</button>
+              <img src={film.image} alt={film.title} className='card-banner' />
+              <div className='card-content'>
+                <div className='card-title-container'>
+                  <h2 className='card-title'>{film.title}</h2>
+                  <p className='card-director'>{film.director}</p>
+                </div>
+                <div className='card-score-container'>
+                  <p className='card-score'>{film.rt_score}</p>
+                </div>
               </div>
-              <div className='card-score-container'>
-                <p className='card-score'>{film.rt_score}</p>
-              </div>
+              <DetailsButton filmId={film.id} />
             </div>
-          </div>
           ))
         }
       </div>
