00af9bdb22323c4e651de230ddc92e64570652cc
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-13 01:35:29 -0300

    feat: estiliza pagina not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,11 @@
+import notFound from '../../assets/not-found.svg';
+import './styles.css';
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="img-not-found">
+      <img src={notFound} alt="Not Found" />
+    </div>
   );
 }
 
